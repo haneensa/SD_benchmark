6c2e1108d7f5d9463cc18c6947798fe3b501d256
Haneen Mohammed <hamohammed.sagmail.com>
2023-03-15 18:36:52 -0400

    update

--- plotting/micro.py
+++ plotting/micro.py
@@ -4,23 +4,27 @@ from pygg import *
 
 # for each query, 
 def overhead(base, extra):
-    return (extra-base)*100
+    return (extra-base)*1000
+
+def relative_overhead(base, extra):
     return max(((extra-base)/base)*100, 0)
 
+def fanout(a, b):
+    if b == 0: return 0
+    return a / b
     
-lcopy = "feb27_copy"
-lfull = "feb27_full"
+lcopy = "m1_copy"
+lfull = "m1_full"
 
 df_data = pd.read_csv("eval_results/micro_benchmark_notes_feb27_logical.csv")
 df_data["notes"] = "logical"
-temp = pd.read_csv("eval_results/micro_benchmark_notes_feb27_SD.csv")
-df_stats = temp[temp["notes"]=="feb27_stats"]#pd.read_csv("eval_results/micro_benchmark_notes_feb26b_stats.csv")
-temp = temp[temp["notes"]!="feb27_stats"]
+temp = pd.read_csv("eval_results/micro_benchmark_notes_m1.csv")
+df_stats = temp[temp["notes"]=="m1_stats"]#pd.read_csv("eval_results/micro_benchmark_notes_feb26b_stats.csv")
+temp = temp[temp["notes"]!="m1_stats"]
 df_data = df_data.append(temp)
 pd.set_option("display.max_rows", None)
 
 
-df_stats = df_stats.drop(columns=["notes", "output", "runtime"])
 
 # 1. Partition by query time [orderby, filter, filter_scan, perfect_agg, reg_agg, groupby
 #   a. Order by: vary cardinality (1M, 5M, 10M)
@@ -36,59 +40,64 @@ def PlotSelect(filterType):
     df = df.drop(columns=["query", "stats"])
     #df = df[df['lineage_type'] != "Perm"]
     df_Baseline = df[df["lineage_type"]=="Baseline"]
+    df_Baseline = df_Baseline[["cardinality", "groups", "runtime", "output"]]
+    df_Baseline = df_Baseline.rename({'runtime':"Bruntime", 'output': 'Boutput'}, axis=1)
+    df= df[df["lineage_type"]!="Baseline"]
+
     df_withB = pd.merge(df, df_Baseline, how='inner', on = ['cardinality', "groups"])
-    df_withB["roverhead"] = df_withB.apply(lambda x: overhead(x['runtime_y'], x['runtime_x']), axis=1)
-    df_withB= df_withB[df_withB["lineage_type_x"]!="Baseline"]
+    df_withB["roverhead"] = df_withB.apply(lambda x: relative_overhead(x['Bruntime'], x['runtime']), axis=1)
+    df_withB["overhead"] = df_withB.apply(lambda x: overhead(x['Bruntime'], x['runtime']), axis=1)
+    df_withB["fanout"] = df_withB.apply(lambda x: fanout(x['output'],float(x['Boutput'])), axis=1)
     
 
     # full = copy + capture
     # noCapture = copy
     # capture = full - copy
     #print(df_withB.groupby(['cardinality', 'groups', 'lineage_type_x', 'notes_x']).mean())
-    df_copy = df_withB[df_withB['notes_x'] == lcopy]
-    df_full = df_withB[df_withB['notes_x'] == lfull]
-    df_fc = pd.merge(df_copy, df_full, how='inner', on = ['cardinality', "groups"])
-    df_fc = df_fc.rename({'roverhead_y': 'full', 'roverhead_x': 'copy', 'output_y_x': 'output'}, axis=1)
-    df_fc = df_fc[["full", "copy", "output", "cardinality", "groups"]]
-    df_fc["capture"] = df_fc.apply(lambda x: x['full']- x['copy'], axis=1)
-    #print(df_fc.groupby(['cardinality', 'groups']).mean())
+    df_copy = df_withB[df_withB['notes'] == lcopy]
+    df_full = df_withB[df_withB['notes'] == lfull]
     
+    df_full = df_full.drop(columns=["lineage_type", "output", "fanout"])
+    df_copy = df_copy.drop(columns=["lineage_type", "fanout", "output", "Boutput", "Bruntime", "fanout"])
+
+    df_full = df_full.rename({'roverhead': 'roverhead_f','overhead':'overhead_f', 'runtime':"runtime_f" }, axis=1)
+    df_copy = df_copy.rename({'roverhead': 'roverhead_c','overhead':'overhead_c', 'runtime':"runtime_c" }, axis=1)
+    df_fc = pd.merge(df_copy, df_full, how='inner', on = ['cardinality', "groups"])
+
     def normalize(full, nchunks):
         #full *= 100
         if nchunks == 0:
             return full
         else:
             return full/nchunks
-    df_fcstats = pd.merge(df_fc, df_stats, how='inner', on = ['cardinality', "groups"])
-    df_fcstats["full_overhead_nor"] = df_fcstats.apply(lambda x: normalize(x['full'],float(x['stats'].split(',')[1])), axis=1)
-    df_fcstats["capture_overhead_nor"] = df_fcstats.apply(lambda x: normalize(x['capture'],float(x['stats'].split(',')[1])), axis=1)
-    df_fcstats["copy_overhead_nor"] = df_fcstats.apply(lambda x: normalize(x['copy'],float(x['stats'].split(',')[1])), axis=1)
+    df_statsq = df_stats[df_stats['query'] == filterType]
+    df_statsq = df_statsq[["stats", "cardinality", "groups"]]#.drop(columns=["notes", "output", "runtime"])
+    df_fcstats = pd.merge(df_fc, df_statsq, how='inner', on = ['cardinality', "groups"])
+    df_fcstats["foverhead_nor"] = df_fcstats.apply(lambda x: normalize(x['overhead_f'],float(x['stats'].split(',')[1])), axis=1)
+    df_fcstats["coverhead_nor"] = df_fcstats.apply(lambda x: normalize(x['overhead_c'],float(x['stats'].split(',')[1])), axis=1)
+    df_fcstats["froverhead_nor"] = df_fcstats.apply(lambda x: normalize(x['roverhead_f'],float(x['stats'].split(',')[1])), axis=1)
+    df_fcstats["croverhead_nor"] = df_fcstats.apply(lambda x: normalize(x['roverhead_c'],float(x['stats'].split(',')[1])), axis=1)
     df_fcstats["size"] = df_fcstats.apply(lambda x: float(x['stats'].split(',')[0])/(1024.0*1024.0), axis=1)
-    df_fcstats = df_fcstats[["stats", "size", "output", "full_overhead_nor", "capture_overhead_nor", "copy_overhead_nor", "cardinality", "groups"]]
+    df_fcstats["nchunks"] = df_fcstats.apply(lambda x: float(x['stats'].split(',')[1]), axis=1)
+    df_fcstats = df_fcstats.drop(columns=["stats", "notes_x", "notes_y"])
     
-    perm_overhead = df_withB[df_withB["lineage_type_x"]=="Perm"]["roverhead"].aggregate(["mean", "min", "max"])
-    sd_overhead = df_fc["full"].aggregate(["mean", "min", "max"])
-    print("Perm: ", perm_overhead)
-    print("SD: ", sd_overhead)
-    print("Speedup: ", perm_overhead / sd_overhead)
+    perm_overhead = df_withB[df_withB["lineage_type"]=="Perm"].aggregate(["mean", "min", "max"])
+    #perm_overhead = df_withB[df_withB["lineage_type"]=="Perm"].groupby(["cardinality"]).aggregate(["mean", "min", "max"])
+    print("Perm: ", perm_overhead[ ["overhead", "fanout", "roverhead", "runtime", "Bruntime"] ]) 
+    sd_overhead = df_fcstats.aggregate(["mean", "min", "max"])
+    #sd_overhead = df_fcstats.groupby("cardinality").aggregate(["mean", "min", "max"])
+    print("SD: ", sd_overhead[['overhead_f', 'overhead_c', 'runtime_c', 'runtime_f', 'roverhead_c', 'roverhead_f', 'coverhead_nor', 'foverhead_nor']])
+    print("Speedup: ", perm_overhead['runtime'] / sd_overhead['runtime_f'])
 
-    keys = ["full_overhead_nor", "capture_overhead_nor", "copy_overhead_nor", "size"]
-    for k in keys:
-        # compute speedup
-        summary = df_fcstats[k].aggregate(['mean', 'min','max'])
-        #print(k, "--->", summary)
-    summary = df_fcstats.groupby(["cardinality"])["full_overhead_nor"].aggregate(['mean', 'min','max'])
-    #print(k, "--->", summary)
-    df_fc = pd.merge(df_fc, df_fcstats, how='inner', on = ['cardinality', "groups"])
-    for index, row in df_fc.iterrows():
-        vals = ["full", "copy", "capture"]
+    for index, row in df_fcstats.iterrows():
+        vals = ['f', 'c']
         for v in vals:
-            data.append(dict(system="SD", g=row["groups"], nor=row[v+"_overhead_nor"], card=row["cardinality"], ltype=v, overhead=row[v], gcard=str(row["cardinality"])+"~"+str(row["groups"]), optype=filterType))
+            data.append(dict(system="SD", g=row["groups"], nor=row[v+"overhead_nor"], card=row["cardinality"], ltype=v, overhead=row['roverhead_'+v], gcard=str(row["cardinality"])+"~"+str(row["groups"]), optype=filterType))
             #data.append(dict(system="SD", nchunks=row['stats'].split(',')[1], g=row["groups"], card=row["cardinality"], ltype=v, overhead=10, gcard=str(row["cardinality"])+"~"+str(row["groups"]), optype=filterType))
     
     for index, row in df_withB.iterrows():
-        if (row["lineage_type_x"] == "Perm"):
-            data.append(dict(system="Perm", ltype="Perm", overhead=int(row["roverhead"]), gcard=str(row["cardinality"])+"~"+str(row["groups"]), optype=filterType))
+        if (row["lineage_type"] == "Perm"):
+            data.append(dict(system="Perm", ltype="Perm", overhead=row["overhead"], gcard=str(row["cardinality"])+"~"+str(row["groups"]), optype=filterType))
     
     
 
@@ -112,7 +121,7 @@ PlotSelect("filter_scan")
 p = ggplot(data, aes(x='ltype', y='overhead', color='ltype', fill='ltype', group='ltype', shape='ltype'))
 p += geom_bar(stat=esc('identity'), alpha=0.8, width=0.5)# + coord_flip()
 p += facet_wrap("~optype~gcard", scales=esc("free_y"))
-ggsave("micro_overhead_scans.png", p,  width=10, height=10)
+ggsave("micro_overhead_scans.png", p,  width=15, height=10)
 alldata.extend(data)
 
 data = []
--- plotting/tpch_capture.py
+++ plotting/tpch_capture.py
@@ -23,14 +23,17 @@ legend_bottom = legend + theme(**{
 }),
 # for each query, 
 def relative_overhead(base, extra): # in %
-    return max(((extra-base)/base)*100, 0)
+    return max(((float(extra)-float(base))/float(base))*100, 0)
 
 def overhead(base, extra): # in ms
-    return max(((extra-base))*1000, 0)
+    return max(((float(extra)-float(base)))*1000, 0)
 
-df = pd.read_csv("eval_results/tpch_benchmark_capture_feb28.csv")
-df = df[df["sf"] == 1]
+df = pd.read_csv("eval_results/tpch_benchmark_capture_m1_SD.csv")
+df_stats = df[df["notes"]=="m1_stats"]
+df = df[df["notes"]!="m1_stats"]
+df = df[df["sf"]==1]
 pd.set_option("display.max_rows", None)
+#pd.set_option("display.max_columns", None)
 
 data = []
 
@@ -40,26 +43,52 @@ df_Baseline = df_Baseline[["runtime", "query", "sf", "n_threads", "output"]]
 df_withB = pd.merge(df, df_Baseline, how='inner', on = ['query', 'sf', 'n_threads'])
 df_withB["rel_overhead"] = df_withB.apply(lambda x: relative_overhead(x['runtime_y'], x['runtime_x']), axis=1)
 df_withB["overhead"] = df_withB.apply(lambda x: overhead(x['runtime_y'], x['runtime_x']), axis=1)
-df_withB["outputFan"] = df_withB.apply(lambda x: x['output_x']/ x['output_y'], axis=1)
+df_withB["outputFan"] = df_withB.apply(lambda x: float(x['output_x'])/ float(x['output_y']), axis=1)
 df_withB= df_withB[df_withB["lineage_type"]!="Baseline"]
-print(df_withB)
-df_full = df_withB[df_withB["notes"] == "feb28_full"]
-df_copy = df_withB[df_withB["notes"] == "feb28_copy"]
-df_fc = pd.merge(df_copy, df_full, how='inner', on = ['query', "sf", 'n_threads'])
+#print(df_withB)
+
+df_full = df_withB[df_withB["notes"] == "m1_full"]
+df_copy = df_withB[df_withB["notes"] == "m1_copy"]
+df_logical = df_withB[df_withB["lineage_type"] == "Logical-RID"]
+df_logical = df_logical[["query", "sf", "n_threads", "rel_overhead", "overhead", "outputFan", "runtime_x"]]
+df_full = df_full[["query", "sf", "n_threads", "rel_overhead", "notes", "overhead",  "runtime_x", "runtime_y"]]
+#df_copy = df_copy[["query", "sf", "n_threads", "rel_overhead", "notes", "overhead", "runtime_x"]]
+#df_fc = pd.merge(df_copy, df_full, how='inner', on = ['query', "sf", 'n_threads'])
+df_fc = pd.merge(df_full, df_logical, how='inner', on = ['query', "sf", 'n_threads'])
+
+def normalize(full, nchunks):
+    #full *= 100
+    if nchunks == 0:
+        return full
+    else:
+        return full/nchunks
+
+df_stats = df_stats[["stats", "query", "sf", 'n_threads']]
+
+df_fcstats = pd.merge(df_fc, df_stats, how='inner', on = ['query', 'sf', 'n_threads'])
+df_fcstats["full_overhead_nor"] = df_fcstats.apply(lambda x: normalize(x['overhead_y'],float(x['stats'].split(',')[1])), axis=1)
+df_fcstats["copy_overhead_nor"] = df_fcstats.apply(lambda x: normalize(x['overhead_x'],float(x['stats'].split(',')[1])), axis=1)
+df_fcstats["nchunks"] = df_fcstats.apply(lambda x: float(x['stats'].split(',')[1]), axis=1)
+df_fcstats["size"] = df_fcstats.apply(lambda x: float(x['stats'].split(',')[0])/(1024*1024), axis=1)
+print(df_fcstats)
+print(df_fcstats.groupby(["query", "sf", 'n_threads']).mean())
+
 for index, row in df_withB.iterrows():
-    name = row['lineage_type'] + row["notes"]
-    data.append(dict(system=name, overhead=row["overhead"], rel_overhead=row["rel_overhead"], qid=row['query']))
+    if (row["notes"] == "m1_copy"):
+        continue
+    name = row['lineage_type']# + row["notes"]
+    data.append(dict(system=name, notes=row["notes"], overhead=row["overhead"], rel_overhead=row["rel_overhead"], qid=row['query']))
 
 p = ggplot(data, aes(x='qid', y='overhead', color='system', fill='system', group='system', shape='system'))
 p += geom_bar(stat=esc('identity'), alpha=0.8, position=position_dodge(width=0.6), width=0.5)
 p += axis_labels('Query', "Runtime Overhead (ms)", "discrete", "log10")
 #p += ylim(lim=[0,300])
 p += legend_bottom
-ggsave("tpch_overhead.png", p,  width=6, height=3)
+ggsave("tpch_overhead.png", p,  width=6, height=4)
 
 p = ggplot(data, aes(x='qid', y='rel_overhead', color='system', fill='system', group='system', shape='system'))
 p += geom_bar(stat=esc('identity'), alpha=0.8, position=position_dodge(width=0.6), width=0.5)
 p += axis_labels('Query', "Runtime Relative Overhead % (log)", "discrete", "log10")
 #p += ylim(lim=[0,300])
 p += legend_bottom
-ggsave("tpch_relative_overhead.png", p,  width=6, height=3)
+ggsave("tpch_relative_overhead.png", p,  width=6, height=4)
